36100db9f34245fee31143bf99e14526507d284c
DANNJUGUNA <ndan30071@gmail.com>
2022-12-03 13:09:44 +0300

    lab completed

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 function fetchBooks() {
   // To pass the tests, don't forget to return your fetch!
-  
+  return fetch("https://anapioficeandfire.com/api/books")
+  .then((resp) => resp.json())
+  .then(renderBooks);
 }
 
 function renderBooks(books) {
+  
   const main = document.querySelector('main');
   books.forEach(book => {
     const h2 = document.createElement('h2');
